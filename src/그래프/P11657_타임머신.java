42af25d6dc9f5fb16bdfd68f2a296247b178e5d7
seongwon-ha <djfqks22@naver.com>
2023-03-12 22:56:46 +0900

    벨만포트

---
+++ src/그래프/P11657_타임머신.java
@@ -0,0 +1,10 @@
+package 그래프;
+
+import java.util.*;
+import java.io.*;
+
+public class P11657_타임머신 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N, M;
+}
